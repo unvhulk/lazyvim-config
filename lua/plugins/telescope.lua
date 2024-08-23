d754103b4c3b2bde1923725b25261b90e2ad317c
uday.nv <uday.nv@intechadditive.com>
2024-08-23 17:21:38 +0530

    Feat: Ignore node-modules dir

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,30 @@
 return {
   "nvim-telescope/telescope.nvim",
   opts = function(_, opts)
-    -- Set up your custom find_command
     opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
-      find_command = { "rg", "--ignore", "--hidden", "--files", "-u" }, -- Custom find command
+      find_command = {
+        "rg",
+        "--ignore",
+        "--hidden",
+        "--files",
+        "-u",
+        "--glob",
+        "!*/node_modules/*", -- Exclude node_modules from search
+      },
     })
 
-    -- Custom keymap to use your find_files with the modified find_command
+    -- Custom keymap to use the modified find_files command
     vim.keymap.set("n", "<leader>ff", function()
-      -- Use find_files picker with the custom find_command
       require("telescope.builtin").find_files({
-        find_command = { "rg", "--ignore", "--hidden", "--files", "-u" },
+        find_command = {
+          "rg",
+          "--ignore",
+          "--hidden",
+          "--files",
+          "-u",
+          "--glob",
+          "!*/node_modules/*", -- Exclude node_modules
+        },
       })
     end, { noremap = true, silent = true })
 
