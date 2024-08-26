9d8717cf07fedb927a763dce931228d4e36550d4
uday.nv <uday.nv@intechadditive.com>
2024-08-26 16:46:20 +0530

    Fix: Keymap for moving tabs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local opts = { noremap = true, silent = true }
 
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("t", "jk", "<C-\\><C-n>")
@@ -24,22 +25,20 @@ vim.keymap.set("n", "<Leader>n", "<cmd>Noice<cr>")
 -- VS CODE KEYMAPS
 vim.keymap.set("n", "<A-J>", "yyp")
 vim.keymap.set("n", "<A-K>", "yyp")
+
 -- Map Ctrl+Enter to add a new line below the current line and stay in normal mode
-vim.keymap.set("n", "<C-M>", "o<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-M>", "o<Esc>", opts)
 vim.keymap.set("n", "<C-A-a>", "ggVG")
 vim.keymap.set("n", "<C-A-a>", "ggVG")
 vim.keymap.set("n", "<Leader>y", "*y")
 vim.keymap.set("n", "<Leader>p", "*p")
 
 --Reopen last closed buffer/file
-vim.keymap.set("n", "<Leader>bh", "<cmd>e#<cr>", { noremap = true, silent = true })
-
-local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<Leader>bh", "<cmd>e#<cr>", opts)
 
 -- Move tabs
-vim.keymap.set("n", "<C-h>", ":BufferMovePrevious<CR>", opts)
-vim.keymap.set("n", "<C-l>", ":BufferMoveNext<CR>", opts)
+vim.keymap.set("n", "<S-Left>", ":BufferMovePrevious<CR>", opts) -- Shift + Left Arrow for previous buffer
+vim.keymap.set("n", "<S-Right>", ":BufferMoveNext<CR>", opts) -- Shift + Right Arrow for next buffer
 
 -- Cycle through tabs
 vim.keymap.set("n", "H", ":BufferPrevious<CR>", opts)
