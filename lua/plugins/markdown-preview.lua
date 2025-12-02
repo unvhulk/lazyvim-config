9f02324f488d68c4fdfb6d69012575ec85d27ab8
Uday.nv <uday.nv@intechadditive.com>
2025-12-02 11:10:39 +0530

    Fix: Removes openCode and dependencies

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,13 +1,5 @@
 -- Markdown preview in browser
 return {
   "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = "cd app && npm install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  keys = {
-    { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
-  },
 }
